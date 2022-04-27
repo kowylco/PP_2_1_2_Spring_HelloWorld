9a8d7c7a95718bd20ed32ce4a2ab50ce75a33d9a
kowlco <ptoxas@gmail.com>
2022-04-27 18:46:05 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Is HelloWorlds equal? : " + (bean == bean1));
+
+        Cat catBean0 = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Is cats equal? : " + (catBean0 == catBean1));
     }
 }
